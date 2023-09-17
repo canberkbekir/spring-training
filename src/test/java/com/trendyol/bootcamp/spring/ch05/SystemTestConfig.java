e45aca849615580747fe24406818ab03e5d41f89
canberkbekir <boom.canberk@gmail.com>
2023-09-17 20:53:47 +0300

    All test passed

--- src/test/java/com/trendyol/bootcamp/spring/ch05/SystemTestConfig.java
+++ src/test/java/com/trendyol/bootcamp/spring/ch05/SystemTestConfig.java
@@ -1,5 +1,6 @@
 package com.trendyol.bootcamp.spring.ch05;
 
+import com.trendyol.bootcamp.spring.ch05.config.AspectsConfig;
 import com.trendyol.bootcamp.spring.ch05.config.RewardsConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,8 +15,9 @@ import javax.sql.DataSource;
  * Save all your work, run the LoggingAspectTests.  It should pass, 
  * and you should see one line of LoggingAspect output in the console.	 
  */
+
 @Configuration
-@Import({RewardsConfig.class})
+@Import({RewardsConfig.class, AspectsConfig.class})
 public class SystemTestConfig {
 
 	
